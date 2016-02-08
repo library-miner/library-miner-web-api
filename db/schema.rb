32afd10b55fe2f608e9fdb9bcbd9b6bd8dae7532
r_nishio <quick.xp@me.com>
2016-02-08 23:46:41 +0900

    DBスキーマ追加 project_dependency project_readme

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,23 @@
 
 ActiveRecord::Schema.define(version: 0) do
 
+  create_table "project_dependencies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT COMMENT='プロジェクト依存関係'" do |t|
+    t.integer  "project_from_id", null: false
+    t.integer  "project_to_id"
+    t.string   "library_name",    null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["project_from_id"], name: "index_project_dependencies_on_project_from_id", using: :btree
+  end
+
+  create_table "project_readmes", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT COMMENT='プロジェクト_README'" do |t|
+    t.integer  "project_id",               null: false
+    t.text     "content",    limit: 65535, null: false
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+    t.index ["project_id"], name: "project_readmes_project_id_fk", using: :btree
+  end
+
   create_table "projects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin ROW_FORMAT=COMPACT COMMENT='プロジェクト基本情報'" do |t|
     t.boolean  "is_incomplete",                    default: true,  null: false
     t.integer  "github_item_id",     limit: 8
@@ -42,4 +59,5 @@ ActiveRecord::Schema.define(version: 0) do
     t.index ["github_item_id"], name: "index_projects_on_github_item_id", unique: true, using: :btree
   end
 
+  add_foreign_key "project_readmes", "projects", name: "project_readmes_project_id_fk"
 end
--- db/schema/schema.rb
+++ db/schema/schema.rb
@@ -31,4 +31,28 @@ create_table 'projects', collate: 'utf8_bin', comment: 'プロジェクト基本
   t.index :full_name, unique: false
 end
 
+create_table 'project_dependencies', collate: 'utf8_bin', comment: 'プロジェクト依存関係' do |t|
+  t.int :id, comment: 'Id', primary_key: true, extra: :auto_increment
+
+  t.int :project_from_id
+  t.int :project_to_id, null: true
+  t.varchar :library_name
+
+  t.index :project_from_id, unique: false
+
+  t.datetime :created_at
+  t.datetime :updated_at
+end
+
+create_table 'project_readmes', collate: 'utf8_bin', comment: 'プロジェクト_README' do |t|
+  t.int :id, comment: 'Id', primary_key: true, extra: :auto_increment
+  t.int :project_id, comment: 'Project id'
+
+  t.text :content, comment: 'ファイル内容'
+  t.foreign_key 'project_id', reference: 'projects', reference_column: 'id'
+
+  t.datetime :created_at
+  t.datetime :updated_at
+end
+
 
