5c621dc4dd08ed6fdddc00da8db941d72671a8fd
r_nishio <quick.xp@me.com>
2016-02-08 23:33:46 +0900

    Minerよりデータ取得とデータ連携完了通知 追加

--- lib/miner_client.rb
+++ lib/miner_client.rb
@@ -4,9 +4,26 @@ class MinerClient
   MINER_PROJECT_READY_URL = 'api/project_export/export_ready'
   MINER_PROJECT_END_URL = 'api/project_export/export_end'
 
+  # Miner より Webデータ連携準備
   def import_ready(count: 100)
     path = "#{MINER_PROJECT_READY_URL}?count=#{count}"
     Rails.logger.info("MinerClient Access to #{path} - project ready")
+
+    MinerProjectResponse.parse(get_request_to(path))
+  end
+
+  # Miner より データ取得
+  def import_project(page: 1)
+    path = "#{MINER_PROJECT_EXPORT_URL}?page=#{page}"
+    Rails.logger.info("MinerClient Access to #{path} - project import page:" + page.to_s)
+
+    MinerProjectResponse.parse(get_request_to(path))
+  end
+
+  # Miner に Web連携完了通知
+  def import_end
+    path = "#{MINER_PROJECT_END_URL}"
+    Rails.logger.info("MinerClient Access to #{path} - project end")
 
     MinerProjectResponse.parse(get_request_to(path))
   end
