7bf8b08bfa73d97468943908180496154ae2878e
Shinsuke Nishio <nishio@densan-labs.net>
2016-03-05 06:13:02 +0000

    setup script dbcreate 追加; bundle install追加

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# bundle install
+bundle install
+
 # setup database settings
 cp config/{database.yml.example,database.yml}
+
+# setup database
+bundle exec rake db:create db:migrate db:seed
