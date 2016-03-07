5e35db57b2118362f929664a07f6eac44be5dbbb
Shinsuke Nishio <nishio@densan-labs.net>
2016-03-07 16:15:26 +0000

    インデント訂正

--- app/controllers/recent_created_projects_controller.rb
+++ app/controllers/recent_created_projects_controller.rb
@@ -3,7 +3,9 @@ class RecentCreatedProjectsController < ApplicationController
   def index
     @projects = Project.recent_new_projects
 
-    render json: {total_count: @projects.count,
-                  results: @projects}
+    render json: {
+      total_count: @projects.count,
+      results: @projects
+    }
   end
 end
